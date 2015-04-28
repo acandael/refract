9384328f4ce9d224cea7c9eabce7722be0635475
acandael <anthony.candaele@ugent.be>
2015-04-28 09:13:11 +0200

    implement sitemap

--- Gemfile
+++ Gemfile
@@ -12,3 +12,5 @@ gem "wdm", "~> 0.1.0", :platforms => [:mswin, :mingw]
 
 # Windows does not come with time zone data
 gem "tzinfo-data", platforms: [:mswin, :mingw]
+
+gem 'middleman-search_engine_sitemap', '~> 1.3.0'
--- config.rb
+++ config.rb
@@ -53,6 +53,10 @@ set :js_dir, 'javascripts'
 
 set :images_dir, 'images'
 
+set :url_root, 'http://www.refract.eu'
+
+activate :search_engine_sitemap
+
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
